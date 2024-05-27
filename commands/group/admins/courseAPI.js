e7262efb75ab29f0a826a7556fd66c311b9901bb
Jaikishan Bagul <78461084+Script-Kiddie-JKB@users.noreply.github.com>
2024-05-27 17:46:21 +0530

    [TESTING] Random time

--- commands/group/admins/courseAPI.js
+++ commands/group/admins/courseAPI.js
@@ -45,99 +45,89 @@ fetchAndStoreCourses();
 const fetchInterval = setInterval(fetchAndStoreCourses, 5 * 60 * 1000);
 // const fetchInterval = setInterval(fetchAndStoreCourses, 30 * 1000);
 
+const getRandomDelay = () => Math.floor(Math.random() * (10 * 60 * 1000 - 1 * 60 * 1000) + 1 * 60 * 1000); // Random delay between 1 and 10 minutes
+
 const sendCoursesFromDB = async (sock, msg, from, args, msgInfoObj) => {
     const { sendMessageWTyping, groupMetadata } = msgInfoObj;
-    const delay_seconds = Math.floor(Math.random() * 10 + 1) * 60; // Random delay between 1 to 10 minutes
+    const courses = await coursesCollection.find({}).toArray();
+    console.log(`📚 Total courses available: ${courses.length}`);
+    let totalPosted = 0; // Variable to store the total number of posted courses
 
-    try {
-        const courses = await coursesCollection.find({}).toArray();
-        console.log(`📚 Total courses available: ${courses.length}`);
-        let totalPosted = 0; // Variable to store the total number of posted courses
-
-        for (const value of courses) {
-            try {
-                // Check if the course URL is in the cache
-                if (postedCoursesCache.has(value.url)) {
-                    console.log(`🔍 Course '${value.name}' with ID ${value._id} has already been posted`);
-                    continue; // Skip this course
-                }
-
-                // Check if the course has already been posted in the same group
-                const isPosted = await postedCoursesCollection.findOne({ courseId: value._id, groupJid: from });
-
-                if (isPosted) {
-                    console.log(`🔍 Course '${value.name}' with ID ${value._id} has already been posted in group '${isPosted.grpName}'`);
-                    // Add course URL to cache
-                    postedCoursesCache.add(value.url);
-                    continue; // Skip this course
-                }
-
-                const shortLink = await TinyURL.shorten(value.url);
-
-                const sendMessage = async () => {
-                    if (value.image) {
-                        const response = await axios({
-                            url: value.image,
-                            method: 'GET',
-                            responseType: 'arraybuffer'
-                        });
-
-                        await sendMessageWTyping(from, {
-                            image: response.data,
-                            caption: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${shortLink}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
-                        });
-                    } else {
-                        await sendMessageWTyping(from, {
-                            text: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${shortLink}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
-                        });
-                    }
-                };
-
-                await sendMessage();
-                await new Promise(resolve => setTimeout(resolve, delay_seconds * 1000)); // Wait for the specified delay
-
-                // Store information about the posted course including the group name
-                await postedCoursesCollection.insertOne({ courseId: value._id, name: value.name, postedAt: new Date(), groupJid: from, grpName: groupMetadata.subject });
-                console.log(`📢 Posted course '${value.name}' with ID ${value._id} in group '${groupMetadata.subject}' at ${new Date().toLocaleString()}`);
-                totalPosted++;
-
-                postedCoursesCache.add(value.url);
-
-                // Schedule deletion of the posted course from the courses collection after 16 hours
-                // setTimeout(async () => {
-                //     await coursesCollection.deleteOne({ _id: value._id });
-                //     console.log(`🗑️ Deleted posted course '${value.name}' with ID ${value._id} from courses collection after 16 hours`);
-                // }, 16 * 60 * 60 * 1000); // 16 hours
-
-            } catch (err) {
-                console.error('❌ Error:', err);
-                await sendMessageWTyping(from, {
-                    text: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${value.url}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
-                });
+    for (const value of courses) {
+        try {
+            // Check if the course URL is in the cache
+            if (postedCoursesCache.has(value.url)) {
+                console.log(`🔍 Course '${value.name}' with ID ${value._id} has already been posted`);
+                continue; // Skip this course
             }
+
+            // Check if the course has already been posted in the same group
+            const isPosted = await postedCoursesCollection.findOne({ courseId: value._id, groupJid: from });
+
+            if (isPosted) {
+                console.log(`🔍 Course '${value.name}' with ID ${value._id} has already been posted in group '${isPosted.grpName}'`);
+                // Add course URL to cache
+                postedCoursesCache.add(value.url);
+                continue; // Skip this course
+            }
+
+            const shortLink = await TinyURL.shorten(value.url);
+
+            const sendMessage = async () => {
+                if (value.image) {
+                    const response = await axios({
+                        url: value.image,
+                        method: 'GET',
+                        responseType: 'arraybuffer'
+                    });
+
+                    await sendMessageWTyping(from, {
+                        image: response.data,
+                        caption: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${shortLink}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
+                    });
+                } else {
+                    await sendMessageWTyping(from, {
+                        text: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${shortLink}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
+                    });
+                }
+            };
+
+            await sendMessage();
+            const randomDelay = getRandomDelay();
+            await new Promise(resolve => setTimeout(resolve, randomDelay)); // Wait for the random delay
+
+            // Store information about the posted course including the group name
+            await postedCoursesCollection.insertOne({ courseId: value._id, name: value.name, postedAt: new Date(), groupJid: from, grpName: groupMetadata.subject });
+            console.log(`📢 Posted course '${value.name}' with ID ${value._id} in group '${groupMetadata.subject}' at ${new Date().toLocaleString()}`);
+            totalPosted++;
+
+            postedCoursesCache.add(value.url);
+        } catch (err) {
+            console.error('❌ Error:', err);
+            await sendMessageWTyping(from, {
+                text: `📘 *Course:* ${value.name}\n\n📖 *Description:* ${value.shoer_description}\n\n🔗 *Enroll:* ${value.url}\n\n🕒 *Enroll Course Before ${value.sale_end}*`
+            });
         }
-
-        console.log(`🚀 Total courses posted: ${totalPosted}`);
-        await sendMessageWTyping(from, {
-            text: `✅ Successfully posted ${totalPosted} courses! 🎉📚\n\n𝙵𝚘𝚛 𝚖𝚘𝚛𝚎 𝙵𝚛𝚎𝚎 𝙲𝚘𝚞𝚛𝚜𝚎𝚜, 𝙹𝚘𝚒𝚗 𝚞𝚜! \n\n🌟 𝙴𝚡𝚙𝚕𝚘𝚛𝚎 𝚝𝚑𝚎 𝚆𝚘𝚛𝚕𝚍 𝚘𝚏 𝙺𝚗𝚘𝚠𝚕𝚍𝚎𝚐𝚎 𝚠𝚒𝚝𝚑 𝚄𝚜 \n\nhttps://chat.whatsapp.com/LVLRFlxL5T4JMsQFoOaouV`
-        });
-
-        // Delete already posted courses from collections after all processing
-        const postedCourses = await postedCoursesCollection.find({ groupJid: from }).toArray();
-        const postedCourseIds = postedCourses.map(postedCourse => postedCourse.courseId);
-
-        await coursesCollection.deleteMany({ _id: { $in: postedCourseIds } });
-        console.log(`🗑️ Deleted ${postedCourseIds.length} courses from the courses collection`);
-
-        await postedCoursesCollection.deleteMany({ courseId: { $in: postedCourseIds }, groupJid: from });
-        console.log(`🗑️ Deleted ${postedCourseIds.length} courses from the postedCourses collection for group '${groupMetadata.subject}'`);
-
-        // Clear the in-memory cache after processing all courses
-        postedCoursesCache.clear();
-        console.log("🧹 Cleared the in-memory cache");
-    } catch (err) {
-        console.error("❌ Error fetching courses from MongoDB:", err);
     }
+
+    console.log(`🚀 Total courses posted: ${totalPosted}`);
+    await sendMessageWTyping(from, {
+        text: `✅ Successfully posted ${totalPosted} courses! 🎉📚\n\n𝙵𝚘𝚛 𝚖𝚘𝚛𝚎 𝙵𝚛𝚎𝚎 𝙲𝚘𝚞𝚛𝚜𝚎𝚜, 𝙹𝚘𝚒𝚗 𝚞𝚜! \n\n🌟 𝙴𝚡𝚙𝚕𝚘𝚛𝚎 𝚝𝚑𝚎 𝚆𝚘𝚛𝚕𝚍 𝚘𝚏 𝙺𝚗𝚘𝚠𝚕𝚍𝚎𝚐𝚎 𝚠𝚒𝚝𝚑 𝚄𝚜 \n\nhttps://chat.whatsapp.com/LVLRFlxL5T4JMsQFoOaouV`
+    });
+
+    // Delete already posted courses from collections after all processing
+    const postedCourses = await postedCoursesCollection.find({ groupJid: from }).toArray();
+    const postedCourseIds = postedCourses.map(postedCourse => postedCourse.courseId);
+
+    await coursesCollection.deleteMany({ _id: { $in: postedCourseIds } });
+    console.log(`🗑️ Deleted ${postedCourseIds.length} courses from the courses collection`);
+
+    await postedCoursesCollection.deleteMany({ courseId: { $in: postedCourseIds }, groupJid: from });
+    console.log(`🗑️ Deleted ${postedCourseIds.length} courses from the postedCourses collection for group '${groupMetadata.subject}'`);
+
+    // Clear the in-memory cache after processing all courses
+    postedCoursesCache.clear();
+    console.log("🧹 Cleared the in-memory cache");
 };
 
 module.exports.command = () => ({ cmd: ["free", "evil"], handler: sendCoursesFromDB });
